73774f9edc4f10611c4f1e6a2241cef49a677d42
Onur Kucukkece <onurkucukkece@gmail.com>
2019-02-22 11:43:00 +0300

    fixed wrong variable name

--- 2018/day8.rb
+++ 2018/day8.rb
@@ -10,7 +10,7 @@ class NodeParser
     node, meta_entry = @numbers[0], @numbers[1]
     @numbers.shift(2)
     node.times do
-      parse if metadata_entry > 0
+      parse if meta_entry > 0
     end
     @meta << @numbers[0..(meta_entry - 1)]
     @numbers.shift(meta_entry)
