e0e506a6306f046f5b9cc8604f2ab431e8be630b
Onur Kucukkece <onurkucukkece@gmail.com>
2019-02-22 12:19:13 +0300

    updated day 8 specs

--- spec/2018/day8_spec.rb
+++ spec/2018/day8_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe 'Node' do
+  let(:nodes) { NodeParser.new numbers: File.read("#{File.dirname(__FILE__)}/test.txt").split(' ') }
+
   it 'should return node chain' do
-    numbers = File.read("#{File.dirname(__FILE__)}/test8.txt").split(' ')
-    nodes = NodeParser.new(numbers: numbers)
     nodes.parse
     expect(nodes.meta.flatten.sum).to eq 138
+  end
+  
+  it 'should return total of root meta entries' do
+    expect(nodes.parse).to eq 66
   end
 end
